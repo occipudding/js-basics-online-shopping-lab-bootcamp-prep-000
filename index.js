2f371d323fb53ceeaeacbdac54e587524f812570
Joshua Suskin <jsuskin85@gmail.com>
2018-06-15 03:44:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -48,6 +48,9 @@ function removeFromCart(item) {
   for(let i = 0; i < cart.length; i++) {
     if(item === cart[i].itemName) {
       cart.splice(i,1);
+      return cart;
+    } else {
+      return `That item is not in your cart.`;
     }
   }
 }

4c150025ebfda2a0d840336e3914e9a97728a085
Joshua Suskin <jsuskin85@gmail.com>
2018-06-15 01:35:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,26 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ var price = Math.floor(Math.random() * 101 + 1);
+ var newItem = { itemName: item, 
+                 itemPrice: price };
+ cart.push(newItem);
+ return `${item} has been added to your cart.`;
 }
 
 function viewCart() {
-  // write your code here
+  const itemNameAtPrice = [];
+  itemNameAtPrice.push(`${item} at $${price}`);
+  
+  if(!cart.length) {
+     return `Your shopping cart is empty.`;
+  } else if(cart.length === 1) {
+    return `In your cart, you have ${item} at $${price}.`;
+  } else {
+    for(let i = 0; i < cart.length; i++) {
+      
+    }
+  }
 }
 
 function total() {
